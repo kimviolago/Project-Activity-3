d3a9feafc63e2497080a557c6fed8a233dcb7206
batangchucks <kylagail.azucena.iics@ust.edu.ph>
2021-10-14 08:50:04 +0000

    Suggestions on UI

--- mapquest_parse-json_7.py
+++ mapquest_parse-json_7.py
@@ -1,41 +1,77 @@
+from tkinter import*
 import urllib.parse
-import requests 
-
+import requests
 main_api = "https://www.mapquestapi.com/directions/v2/route?"
 key = "UiOM9XuMWPZkCSJtwx2uwj3oCCgdClMv"
+root = Tk()
+root.geometry("1000x2000")
+root.title("Map Quest")
 
-while True:
-    orig = input("Starting Location: ")
-    if orig == "quit" or orig == "q":
+heading = Label(text="Map Quest API", bg="black",
+                fg="white", width="500", height="5")
+heading.pack()
+
+LabelLocation = Label(text="Enter your Current Location* ",
+                      font="Helvetica 15", anchor='nw')
+LabelLocation.pack(pady=20)
+location = Entry(root, width=50)
+location.pack(pady=20)
+
+LabelDestination = Label(
+    text="Enter your Destination Location* ", font="Helvetica 15")
+LabelDestination.pack(pady=20)
+destination = Entry(root, width=50)
+destination.pack(pady=20)
+
+
+def onClick():
+    while True:
+
+        url = main_api + \
+            urllib.parse.urlencode(
+                {"key": key, "from": location, "to": destination})
+        print("URL ", (url))
+        json_data = requests.get(url).json()
+        json_status = json_data["info"]["statuscode"]
+        if json_status == 0:
+            argument = "\nAPI Status: " + str(json_status) + " = A successful route call.\n"\
+                "\n=============================================" + "\nDirections from " + (location.get()) + " to " + (destination.get())\
+                + "\nTrip Duration: " + (json_data["route"]["formattedTime"])\
+                + "\nKilometers: " + str("{:.2f}".format(json_data["route"]["distance"] * 1.6))\
+                + "\nFuel Used (Ltr): " + str("{:.3f}".format(json_data["route"]["fuelUsed"]*3.78))\
+                + "\n============================================="
+            myResult1 = Label(root, text=argument)
+            myResult1.pack()
+            for each in json_data["route"]["legs"][0]["maneuvers"]:
+                argument2 = (each["narrative"]) + " (" + \
+                    str("{:.2f}".format((each["distance"])*1.61) + " km)")
+                myResult2 = Label(root, text=argument2,
+                                  font="Helvetica 10", justify=LEFT)
+                myResult2.pack()
+        elif json_status == 402:
+            argument3 = "**********************************************"\
+                + "Status Code: " + str(json_status) + "; Invalid user inputs for one or bothlocations."\
+                + "**********************************************\n"
+            myLabel3 = Label(root, text=argument3)
+            mylabel3.pack()
+        elif json_status == 611:
+            argument4 = "**********************************************"\
+                + "Status Code: " + str(json_status) + "; Missing an entry for one or bothlocations."\
+                + "**********************************************\n"
+            myLabel4 = Label(root, text=argument4)
+            mylabel4.pack()
+        else:
+            argument5 = "************************************************************************"\
+                + "For Staus Code: " + str(json_status) + "; Refer to:"\
+                + "https://developer.mapquest.com/documentation/directions-api/status-codes"\
+                + "************************************************************************\n"
+            myResult = Label(root, text=argument5)
+            myResult.pack(pady=20)
         break
-    dest = input("Destination: ")
-    if dest == "quit" or dest == "q":
-        break
-    url = main_api + urllib.parse.urlencode({"key":key, "from":orig,"to":dest})
-    print("URL: " + (url))
-    json_data = requests.get(url).json()
-    json_status = json_data["info"]["statuscode"]
-    if json_status == 0:
-        print("API Status: " + str(json_status) + " = A successful route call.\n")
-        print("=============================================") 
-        print("Directions from " + (orig) + " to " + (dest)) 
-        print("Trip Duration: " + (json_data["route"]["formattedTime"])) 
-        print("Kilometers: " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
-        print("Fuel Used (Ltr): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)))
-        print("=============================================")
-        for each in json_data["route"]["legs"][0]["maneuvers"]: 
-            print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)")) 
-        print("=============================================\n")
-    elif json_status == 402: 
-        print("**********************************************") 
-        print("Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.") 
-        print("**********************************************\n") 
-    elif json_status == 611: 
-        print("**********************************************") 
-        print("Status Code: " + str(json_status) + "; Missing an entry for one or both locations.") 
-        print("**********************************************\n") 
-    else:
-        print("************************************************************************") 
-        print("For Staus Code: " + str(json_status) + "; Refer to:") 
-        print("https://developer.mapquest.com/documentation/directions-api/status-codes") 
-        print("************************************************************************\n")
+
+
+myButton = Button(root, text="Submit", command=onClick)
+myButton.pack()
+
+
+root.mainloop()
